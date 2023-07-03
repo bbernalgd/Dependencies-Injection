58f6e4e26d18572ef3f4d84ac6619c1664254cfc
Brenda Bernal <bbernal@griddynamics.com>
2023-07-03 12:27:43 -0500

    Added generic type for ioc container

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -3,9 +3,17 @@ import IoCContainer from "ioc-lite";
 import { Logger } from "../services/logger";
 import { HTTP } from "../services/http";
 import { Users } from "../services/users";
+import {ApiConfig} from "../types/index"
+
+type ioCContainer = {
+  logger: typeof Logger,
+  http: typeof HTTP,
+  users: typeof Users,
+  apiConfig: ApiConfig
+}
 
 export const createIoCContainer = () => {
-  const ioc = new IoCContainer();
+  const ioc = new IoCContainer<ioCContainer>();
 
   ioc.registerClass("logger", Logger);
   ioc.registerClass("http", HTTP);
